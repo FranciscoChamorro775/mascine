bcf4036c15fbec1daf7e1f238fcf4626270ad199
Fran <chamorro_66618@hotmail.com>
2026-04-30 11:44:35 +0200

    Fix: conexión MySQL Railway

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,27 @@
 const mysql = require("mysql2/promise");
-require("dotenv").config();
 
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASS:", process.env.DB_PASS);
+// Solo cargar .env en desarrollo (local)
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
+
+// Debug para comprobar que Railway lee las variables
+console.log("HOST:", process.env.MYSQLHOST);
+console.log("USER:", process.env.MYSQLUSER);
+console.log("PASS:", process.env.MYSQLPASSWORD);
+console.log("DB:", process.env.MYSQLDATABASE);
+console.log("PORT:", process.env.MYSQLPORT);
 
 const db = mysql.createPool({
-    host: "localhost",
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: "mascine"
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT,
+    ssl: {
+        rejectUnauthorized: true
+    }
 });
 
 module.exports = db;
+
